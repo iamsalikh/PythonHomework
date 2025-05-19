7ea2f0c235fd439ca585ebc558c223721910349b
iamsalikh <138610450+iamsalikh@users.noreply.github.com>
2025-05-19 13:36:31 +0500

    Create hw1.py

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,25 @@
+# 1. Given a side of square. Find its perimeter and area.
+side = 4
+p = 4 * side
+print(p)
+
+# 2. Given diameter of circle. Find its length.
+diameter = 10
+pi = 3.14
+length = diameter * pi
+print(length)
+
+# 3. Given two numbers a and b. Find their mean.
+a = 50
+b = 3
+mean = (a + b) / 2
+print(mean)
+
+# 4. Given two numbers a and b. Find their sum, product and square of each number.
+a = 5
+b = 5
+sum = a + b
+product = a * b
+square_a = a**2
+square_b = b**2
+print(sum, product, square_a, square_b)
