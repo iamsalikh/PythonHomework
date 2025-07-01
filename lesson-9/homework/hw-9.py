600064a1d1ed0b482055ebcfa838375e5046ad51
iamsalikh <138610450+iamsalikh@users.noreply.github.com>
2025-07-01 18:02:59 +0500

    Create hw-9.py

---
+++ lesson-9/homework/hw-9.py
@@ -0,0 +1,12 @@
+# 1. Circle Class
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius ** 2 
+    
+    def perimetr(self):
+        return 2 * 3.14 * self.radius
+
+# 2. Person Class
