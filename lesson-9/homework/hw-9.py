fef2d05f8d86a181f62370bdbfcd285444378bf1
iamsalikh <138610450+iamsalikh@users.noreply.github.com>
2025-07-02 14:11:28 +0500

    Update hw-9.py

--- lesson-9/homework/hw-9.py
+++ lesson-9/homework/hw-9.py
@@ -10,3 +10,30 @@ class Circle:
         return 2 * 3.14 * self.radius
 
 # 2. Person Class
+class Person:
+    def __init__(self, name, country, date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth 
+        
+    def get_age(self):
+        return f"{self.name} from {self.country} is {2025 - self.date_of_birth} years old."
+
+# 3. Calculator Class
+class Calculator:
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b != 0:
+            return a / b
+        else:
+            return "Ошибка: деление на ноль"
+
+# 4. Shape and Subclasses
